d007895ffdd8af6fd8acebdf26c7aaf3cd117baa
Nk <nazmulkabir512@gmail.com>
2020-02-14 03:08:01 +0600

    timer set

--- app/src/main/java/com/vectorit/instabooster/Booster_Activity.java
+++ app/src/main/java/com/vectorit/instabooster/Booster_Activity.java
@@ -1,14 +1,96 @@
 package com.vectorit.instabooster;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
-public class Booster_Activity extends AppCompatActivity {
+public class Booster_Activity extends AppCompatActivity implements View.OnClickListener {
+
+    CardView cv_follow;
+    ImageView iv_menu_back;
+    Button btn_start;
+    TextView tv_follow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booster_);
+
+        //Hooks
+        cv_follow = findViewById(R.id.cv_follow);
+        iv_menu_back = findViewById(R.id.menu_images_button);
+        btn_start = findViewById(R.id.btn_start);
+        tv_follow = findViewById(R.id.tv_follow);
+
+
+        //onclick
+        cv_follow.setOnClickListener(this);
+        iv_menu_back.setOnClickListener(this);
+        btn_start.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.cv_follow:
+
+                Uri uri = Uri.parse("http://instagram.com/_u/cristiano/");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.instagram.com/cristiano/")));
+                }
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            tv_follow.setText("");
+                            tv_follow.setBackground(getResources().getDrawable(R.drawable.ic_done_black_24dp));
+                        }
+                        btn_start.setClickable(true);
+                        GradientDrawable myGrad = (GradientDrawable) btn_start.getBackground();
+                        myGrad.setColor( getResources().getColor(R.color.unlock_start));
+                        btn_start.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                    }
+                },3*1000);
+
+                break;
+            case R.id.btn_start:
+                startWatch();
+                break;
+
+            case R.id.menu_images_button:
+                Intent intent = new Intent(Booster_Activity.this, ProfileActivity.class);
+                startActivity(intent);
+                finish();
+
+            default:
+                break;
+        }
+    }
+
+    private void startWatch(){
+        Intent intent = new Intent(Booster_Activity.this, ProfileActivity.class);
+        intent.putExtra("status", "1");
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/vectorit/instabooster/ProfileActivity.java
+++ app/src/main/java/com/vectorit/instabooster/ProfileActivity.java
@@ -1,26 +1,46 @@
 package com.vectorit.instabooster;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener
 {
 
-    CardView cv_help,cv_hashtag,cv_boost;
+    CardView cv_help,cv_hashtag,cv_boost,cv_server;
+    TextView tv_countDown,tv_countDown_caption,tv_boost;
+    int state = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        if(getIntent().hasExtra("status")){
+            String status = getIntent().getStringExtra("status");
+            if(status.equals("1")){
+                countDown();
+            }
+
+        }
+
         //Hooks
         cv_help = findViewById(R.id.cv_help);
         cv_hashtag = findViewById(R.id.cv_hastag);
         cv_boost = findViewById(R.id.cv_boost);
+        tv_countDown = findViewById(R.id.tv_countDown);
+        cv_server = findViewById(R.id.cv_server_stopped);
+        tv_countDown_caption = findViewById(R.id.tv_countDown_caption);
+        tv_boost = findViewById(R.id.tv_boost);
 
         cv_help.setOnClickListener(this);
         cv_hashtag.setOnClickListener(this);
@@ -42,10 +62,40 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             case  R.id.cv_boost:
                 it = new Intent(this,Booster_Activity.class);
                 startActivity(it);
+                finish();
                 break;
 
             default:
                 break;
         }
+    }
+
+    private void countDown(){
+        new CountDownTimer(3000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                tv_countDown_caption.setVisibility(View.VISIBLE);
+                tv_countDown.setVisibility(View.VISIBLE);
+                String time = String.format("%02d:%02d:%02d"
+                        , TimeUnit.MILLISECONDS.toHours(millisUntilFinished)
+                        , TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished))
+                        , TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))
+                );
+                tv_countDown.setText(time);
+
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+            public void onFinish() {
+                tv_countDown_caption.setVisibility(View.GONE);
+                tv_countDown.setVisibility(View.GONE);
+
+                cv_boost.setClickable(false);
+                cv_server.bringToFront();
+
+            }
+
+        }.start();
+
     }
 }
